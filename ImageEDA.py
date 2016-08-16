6371f73e52d51b6fa5c12e57d33fbacb0c0a9e50
tchordia <tchordia@gmail.com>
2016-08-15 20:35:09 -0700

    add itorch and random python testing

---
+++ ImageEDA.py
@@ -0,0 +1,22 @@
+from skimage import data, io, filters, color, measure
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+filename = '/Users/Tanmay/Development/Guernica/train_4/4.jpg'
+
+image = io.imread(filename)
+print type(image)
+print image.shape
+
+hsv = color.rgb2hsv(image)
+io.imshow(hsv[:,:,0])
+io.show();
+
+
+# edges = filters.sobel(image[:,:,1])
+# # io.imshow(image)
+# io.imshow(edges)
+# io.show()
